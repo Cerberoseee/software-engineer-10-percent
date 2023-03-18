c3eaa61bb1a1fceea00dbd56e7e949312b19c414
Cerberose <tranlethaison123@gmail.com>
2023-03-19 02:15:12 +0700

    first push

---
+++ 8. Demo/TicketVendorMachine/TicketVendorMachine/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace TicketVendorMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tripGrp = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.grdTrip = new System.Windows.Forms.DataGridView();
+            this.btnNext1 = new System.Windows.Forms.Button();
+            this.grpTimeSeat = new System.Windows.Forms.GroupBox();
+            this.grdSeat = new System.Windows.Forms.DataGridView();
+            this.grdTime = new System.Windows.Forms.DataGridView();
+            this.btnNext2 = new System.Windows.Forms.Button();
+            this.btnBack1 = new System.Windows.Forms.Button();
+            this.tripGrp.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdTrip)).BeginInit();
+            this.grpTimeSeat.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdSeat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdTime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tripGrp
+            // 
+            this.tripGrp.Controls.Add(this.pictureBox1);
+            this.tripGrp.Controls.Add(this.grdTrip);
+            this.tripGrp.Location = new System.Drawing.Point(27, 12);
+            this.tripGrp.Name = "tripGrp";
+            this.tripGrp.Size = new System.Drawing.Size(1147, 571);
+            this.tripGrp.TabIndex = 0;
+            this.tripGrp.TabStop = false;
+            this.tripGrp.Text = "Choose your desired trip";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(608, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(533, 522);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // grdTrip
+            // 
+            this.grdTrip.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdTrip.Location = new System.Drawing.Point(6, 25);
+            this.grdTrip.Name = "grdTrip";
+            this.grdTrip.RowHeadersWidth = 62;
+            this.grdTrip.RowTemplate.Height = 28;
+            this.grdTrip.Size = new System.Drawing.Size(583, 522);
+            this.grdTrip.TabIndex = 0;
+            this.grdTrip.Click += new System.EventHandler(this.grdTrip_Click);
+            // 
+            // btnNext1
+            // 
+            this.btnNext1.Location = new System.Drawing.Point(1019, 611);
+            this.btnNext1.Name = "btnNext1";
+            this.btnNext1.Size = new System.Drawing.Size(143, 47);
+            this.btnNext1.TabIndex = 1;
+            this.btnNext1.Text = "Next";
+            this.btnNext1.UseVisualStyleBackColor = true;
+            this.btnNext1.Click += new System.EventHandler(this.btnNext1_Click);
+            // 
+            // grpTimeSeat
+            // 
+            this.grpTimeSeat.Controls.Add(this.grdSeat);
+            this.grpTimeSeat.Controls.Add(this.grdTime);
+            this.grpTimeSeat.Location = new System.Drawing.Point(21, 12);
+            this.grpTimeSeat.Name = "grpTimeSeat";
+            this.grpTimeSeat.Size = new System.Drawing.Size(1147, 571);
+            this.grpTimeSeat.TabIndex = 2;
+            this.grpTimeSeat.TabStop = false;
+            this.grpTimeSeat.Text = "Choose your desired depart time and trip";
+            // 
+            // grdSeat
+            // 
+            this.grdSeat.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdSeat.Location = new System.Drawing.Point(554, 25);
+            this.grdSeat.Name = "grdSeat";
+            this.grdSeat.RowHeadersWidth = 62;
+            this.grdSeat.RowTemplate.Height = 28;
+            this.grdSeat.Size = new System.Drawing.Size(587, 522);
+            this.grdSeat.TabIndex = 1;
+            this.grdSeat.Click += new System.EventHandler(this.grdSeat_Click);
+            // 
+            // grdTime
+            // 
+            this.grdTime.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdTime.Location = new System.Drawing.Point(6, 25);
+            this.grdTime.Name = "grdTime";
+            this.grdTime.RowHeadersWidth = 62;
+            this.grdTime.RowTemplate.Height = 28;
+            this.grdTime.Size = new System.Drawing.Size(502, 522);
+            this.grdTime.TabIndex = 0;
+            this.grdTime.Click += new System.EventHandler(this.grdTime_Click);
+            // 
+            // btnNext2
+            // 
+            this.btnNext2.Location = new System.Drawing.Point(1019, 611);
+            this.btnNext2.Name = "btnNext2";
+            this.btnNext2.Size = new System.Drawing.Size(143, 47);
+            this.btnNext2.TabIndex = 3;
+            this.btnNext2.Text = "Next";
+            this.btnNext2.UseVisualStyleBackColor = true;
+            this.btnNext2.Click += new System.EventHandler(this.btnNext2_Click);
+            // 
+            // btnBack1
+            // 
+            this.btnBack1.Location = new System.Drawing.Point(21, 611);
+            this.btnBack1.Name = "btnBack1";
+            this.btnBack1.Size = new System.Drawing.Size(143, 47);
+            this.btnBack1.TabIndex = 4;
+            this.btnBack1.Text = "Back";
+            this.btnBack1.UseVisualStyleBackColor = true;
+            this.btnBack1.Click += new System.EventHandler(this.backBtn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1180, 670);
+            this.Controls.Add(this.btnBack1);
+            this.Controls.Add(this.btnNext2);
+            this.Controls.Add(this.grpTimeSeat);
+            this.Controls.Add(this.btnNext1);
+            this.Controls.Add(this.tripGrp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tripGrp.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdTrip)).EndInit();
+            this.grpTimeSeat.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdSeat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdTime)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox tripGrp;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView grdTrip;
+        private System.Windows.Forms.Button btnNext1;
+        private System.Windows.Forms.GroupBox grpTimeSeat;
+        private System.Windows.Forms.DataGridView grdSeat;
+        private System.Windows.Forms.DataGridView grdTime;
+        private System.Windows.Forms.Button btnNext2;
+        private System.Windows.Forms.Button btnBack1;
+    }
+}
+
---
+++ 8. Demo/TicketVendorMachine/TicketVendorMachine/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicketVendorMachine
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlDataAdapter data;
+        SqlCommand cm;
+        DataTable tb;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        //Load data for the Trip field
+        private void loadTrip()
+        {
+            string s = "select distinct FromLoc, DestLoc from Trip where AvailableSeat > 0";
+            cm = new SqlCommand(s, conn);
+            data = new SqlDataAdapter(cm);
+            tb = new DataTable();
+            data.Fill(tb);
+            grdTrip.DataSource = tb;
+        }
+
+        //Load data for the Time field
+        private void loadTime(String tripFromLoc, String tripDestLoc)
+        {
+            string s = "select DepartTime from Trip where FromLoc = '" + tripFromLoc + "' and DestLoc = '" + tripDestLoc + "'";
+            cm = new SqlCommand(s, conn);
+            data = new SqlDataAdapter(cm);
+            tb = new DataTable();
+            data.Fill(tb);
+            grdTime.DataSource = tb;
+        }
+
+        //Load data for the Seat field
+        private void loadSeat(String tripFromLoc, String tripDestLoc, String tripDepartTime)
+        {
+            string s = "select SeatNumber from Seat inner join Trip on Trip.ID = Seat.TripID " +
+                "where FromLoc = '" + tripFromLoc + "' and DestLoc = '" + tripDestLoc + "' and DepartTime = '" + TimeSpan.Parse(tripDepartTime) + "' and isOccupied = 0";
+            cm = new SqlCommand(s, conn);
+            data = new SqlDataAdapter(cm);
+            tb = new DataTable();
+            data.Fill(tb);
+            grdSeat.DataSource = tb;
+        }
+
+        //Take Action when the form is loading
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Initiating the connection to the database
+            string s = "initial catalog = TicketVendorMachine; data source = CERBEROSE; integrated security = true";
+            conn = new SqlConnection(s);
+            conn.Open();
+            loadTrip();
+            grpTimeSeat.Visible = false;
+            btnNext1.Enabled = false;
+            btnNext2.Visible = false;
+            btnBack1.Visible = false;
+            btnNext2.Enabled = false;
+        }
+
+        //Take Action when the user click on the trip table
+        private void grdTrip_Click(object sender, EventArgs e)
+        {
+            btnNext1.Enabled = true;
+            String tripFromLoc = grdTrip.CurrentRow.Cells[0].Value.ToString();
+            String tripDestLoc = grdTrip.CurrentRow.Cells[1].Value.ToString();
+            loadTime(tripFromLoc, tripDestLoc);
+        }
+
+        //Take Action when the user click on the time table
+        private void grdTime_Click(object sender, EventArgs e)
+        {
+            String tripFromLoc = grdTrip.CurrentRow.Cells[0].Value.ToString();
+            String tripDestLoc = grdTrip.CurrentRow.Cells[1].Value.ToString();
+            String tripDepartTime = grdTime.CurrentRow.Cells[0].Value.ToString();
+            loadSeat(tripFromLoc, tripDestLoc, tripDepartTime);
+        }
+
+        //Take Action when the user click on the next button
+        private void btnNext1_Click(object sender, EventArgs e)
+        {
+            grpTimeSeat.Visible = true;
+            tripGrp.Visible = false;
+            btnNext2.Visible = true;
+            btnNext1.Visible = false;
+            btnBack1.Visible = true;
+        }
+
+        //Take Action when the user click on the back button
+        private void backBtn1_Click(object sender, EventArgs e)
+        {
+            grpTimeSeat.Visible = false;
+            tripGrp.Visible = true;
+            btnNext2.Visible = false;
+            btnNext1.Visible = true;
+            btnBack1.Visible = false;
+            grdSeat.DataSource = null;
+            grdTime.DataSource = null;
+        }
+
+        //Take Action when the user click on the next button
+        private void btnNext2_Click(object sender, EventArgs e)
+        {
+            String fromLoc = grdTrip.CurrentRow.Cells[0].Value.ToString();
+            String destLoc = grdTrip.CurrentRow.Cells[1].Value.ToString();
+            String time = grdTime.CurrentRow.Cells[0].Value.ToString();
+            String seat = grdSeat.CurrentRow.Cells[0].Value.ToString();
+
+            //Show up the confirmation box
+            if (MessageBox.Show(
+                "From: " + fromLoc + "\n" +
+                "Destination: " + destLoc + "\n" +
+                "Depart Time: " + time + "\n" +
+                "Seat: " + seat + "\n" +
+                "Proceed to Checkout?"
+            , "Adding", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                MessageBox.Show("Ticket Added", "Success");
+
+                //Get the current ID for the ticket
+                string s = "select * from Ticket";
+                cm = new SqlCommand(s, conn);
+                data = new SqlDataAdapter(cm);
+                tb = new DataTable();
+                data.Fill(tb);
+                int id = tb.Rows.Count + 1;
+
+                //Get the Trip ID for the ticket
+                s = "select ID from Trip where DestLoc = '" + destLoc + "' and FromLoc = '" + fromLoc +"' and DepartTime = '" + TimeSpan.Parse(time) + "'";
+                cm = new SqlCommand(s, conn);
+                data = new SqlDataAdapter(cm);
+                tb = new DataTable();
+                data.Fill(tb);
+
+                String tripId = tb.Rows[0][0].ToString();
+
+                s = "insert into Ticket\n" +
+                " values ('" + id + "', '" + tripId + "', '"+ seat +"')\n";
+                cm = new SqlCommand(s, conn);
+                cm.ExecuteNonQuery();
+            }
+
+        }
+
+        //Activate the next button when the user complete all their input
+        private void grdSeat_Click(object sender, EventArgs e)
+        {
+            btnNext2.Enabled = true;
+        }
+    }
+}
